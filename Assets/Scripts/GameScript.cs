d04ba0b810703c27c0061fbb7a1405e34d942148
Carlosss3 <carlosrojasfeixas@gmail.com>
2014-10-08 18:53:59 +0200

    Menu Inicial

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Escape)) {
+			Reiniciar ();
+		}
+	}
+
+	public void Salir(){
+		Application.Quit ();
+		}
+
+	public void Reiniciar(){
+		Application.LoadLevel ("Menu");
+		}
+
+	public void Iniciar (){
+		Application.LoadLevel ("Nivel 02");
+
+		}
+}
---
+++ Assets/Scripts/GemaScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemaScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+				Application.LoadLevel ("Nivel 01");
+		}
+}
